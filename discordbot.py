3075c1ccaa2c9f9a4427c32975a5da2b3ba0d091
FugaSakamoto <38383223+FugaSakamoto@users.noreply.github.com>
2021-03-10 20:53:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,26 @@
+
+from discord.ext import commands
 import random
 import asyncio
-import discord
+import os
+import traceback
 
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-  if message.content.startswith("🎰"):
-    kakuritu = random.randint(1, 199)
-    slot_list = [':8_1:', ':8_2:', ':8_3:', ':8_4:', ':8_5:', ':8_6:', ':8_7:']
-    A = random.choice(slot_list)
-    B = random.choice(slot_list)
-    C = random.choice(slot_list)
-    if int(kakuritu) == int(1):
-      await client.send_message(message.channel, "ボーナス確定！！！")
-      await asyncio.sleep(3)
-      await client.send_message(message.channel, ':seven:', ':seven:', ':seven:')
-    else:
-      await client.send_message(message.channel, "%s%s%s" % (A, B, C))
+    if message.content.startswith("🎰"):
+        kakuritu = random.randint(1, 199)
+        slot_list = [':8_1:', ':8_2:', ':8_3:', ':8_4:', ':8_5:', ':8_6:', ':8_7:']
+        A = random.choice(slot_list)
+        B = random.choice(slot_list)
+        C = random.choice(slot_list)
+        if int(kakuritu) == int(1):
+              await client.send_message(message.channel, "ボーナス確定！！！")
+              await asyncio.sleep(3)
+              await client.send_message(message.channel, ':seven:', ':seven:', ':seven:')
+        else:
+              await client.send_message(message.channel, "%s%s%s" % (A, B, C))
 
 client.run(token)

f29d3fb2ea60a107e259b228e18de71576231d70
FugaSakamoto <38383223+FugaSakamoto@users.noreply.github.com>
2021-03-10 20:43:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,16 @@
-from discord.ext import commands
-import os
-import traceback
+import random
+import asyncio #sleepを使うのに必要
+import discord ##discordでBOTを使うのにこれが必ずいる
 
-bot = commands.Bot(command_prefix='🎰')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+if message.content == "スロット":
+kakuritu = random.randint(1, 199)
+slot_list = [':8_1:', ':8_2:', ':8_3:', ':8_4:', ':8_5:', ':8_6:', ':8_7:']
+A = random.choice(slot_list)
+B = random.choice(slot_list)
+C = random.choice(slot_list)
+if int(kakuritu) == int(1): #確率は1 /399
+await client.send_message(message.channel, "ボーナス確定！！！")
+await asyncio.sleep(3) #3秒間待ってやる
+await client.send_message(message.channel, ':seven:', ':seven:', ':seven:') #7だけ出るように指定
+else:
+await client.send_message(message.channel, "%s%s%s" % (A, B, C))
